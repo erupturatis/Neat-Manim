44408d700b4ca420b5f912f76953e43533e61e72
erupturatis <bb.octav@gmail.com>
2022-08-22 17:05:05 +0300

    transofrming groups of neuros into other neurons

---
+++ NeatManim/Main.py
@@ -0,0 +1,4 @@
+
+def NeatVisualisation(scene):
+    def construct(self):
+        pass
--- NeatManim/test.py
+++ NeatManim/test.py
@@ -1,8 +1,23 @@
 
 from cProfile import label
+from tokenize import Number
+from turtle import circle
 from manim import *
 class OpeningManim(Scene):
     def construct(self):
-        ax = Axes(x_range=(-6,6), y_range=(-20,20))
-        curve = ax.plot( lambda x: x**3)
-        self.add(ax, curve)
+        inputs = VGroup(*[Circle(radius=0.05, ) for _ in range(5)])
+        inputs.arrange(UP, buff=0.05)
+        inputs2 = VGroup(*[Circle(radius=0.05, ) for _ in range(5)])
+        inputs2.arrange(LEFT, buff=0.05)
+        inputs3 = VGroup(*[Circle(radius=0.05, ) for _ in range(10)])
+        inputs3.arrange(LEFT)
+        inputs.to_edge(UP, buff=0.5)
+        inputs2.to_edge(LEFT, buff=0.5)
+        inputs4 = VGroup(inputs, inputs2)
+        self.add(inputs4, inputs3)
+        
+        self.play(Transform(inputs4, inputs3))
+
+        self.wait()
+
+
