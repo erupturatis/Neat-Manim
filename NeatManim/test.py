1c02f3c68bae33c6474b3ed4613d205a874f1ce0
erupturatis <bb.octav@gmail.com>
2022-08-29 15:33:38 +0300

    manim

--- NeatManim/test.py
+++ NeatManim/test.py
@@ -5,19 +5,12 @@ from turtle import circle
 from manim import *
 class OpeningManim(Scene):
     def construct(self):
-        inputs = VGroup(*[Circle(radius=0.05, ) for _ in range(5)])
-        inputs.arrange(UP, buff=0.05)
-        inputs2 = VGroup(*[Circle(radius=0.05, ) for _ in range(5)])
-        inputs2.arrange(LEFT, buff=0.05)
-        inputs3 = VGroup(*[Circle(radius=0.05, ) for _ in range(10)])
-        inputs3.arrange(LEFT)
-        inputs.to_edge(UP, buff=0.5)
-        inputs2.to_edge(LEFT, buff=0.5)
-        inputs4 = VGroup(inputs, inputs2)
-        self.add(inputs4, inputs3)
-        
-        self.play(Transform(inputs4, inputs3))
 
+        polys = RegularPolygon(5, radius=1, fill_opacity = 0.5, color=RED)
+        self.play(DrawBorderThenFill(polys), run_time=2)
+        self.play(
+            polys.animate.shift(UP)
+        )
         self.wait()
 
 
