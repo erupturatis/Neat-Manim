4f0df4967134af694b5625aa23fe59fac2073bf4
erupturatis <bb.octav@gmail.com>
2022-09-25 16:11:16 +0300

    connections about to work

--- NeatManim/manim_visualization.py
+++ NeatManim/manim_visualization.py
@@ -125,13 +125,14 @@ class VisualizeNetworksRefactored(Scene):
     def update_line(self,line:Line, neuron1:Circle, neuron2:Circle):
         # sets and updater for connections passed
         def shifter(mob, dt):
-            mob.put_start_and_end_on(get_corresponding_anchors(neuron1,neuron2))
+            mob.put_start_and_end_on(*get_corresponding_anchors(neuron1,neuron2))
         line.add_updater(shifter)
 
     def construct(self):
         winners = utils.animate_winners()
         winners_inner_neuron_layers = list()
         winners_line_connections = list()
+        winners_line_connections_numerical = list()
         winners_networks_layers = list()
         neurons_maps = list()
         # All the winners should have the same config therefore the same inputs and outputs
@@ -166,8 +167,6 @@ class VisualizeNetworksRefactored(Scene):
             start_layer_munit =  -step / 2 *(num_layers - 1)
             end_layer_munit = step / 2 *(num_layers - 1)
             neurons_layers = list()
-            
-            
    
             for layer_num,layer in enumerate(network_layers):
                 num_neurons = len(layer)
@@ -182,7 +181,46 @@ class VisualizeNetworksRefactored(Scene):
               
                     self.add(neurons)
 
-                neurons_layers.append(neurons)
+                neurons_layers.append(neurons)\
+
+            maximal_weight = 0
+            for connection in connections:
+                input, output, weight = connection
+                maximal_weight = max(maximal_weight,abs(weight))
+
+            line_connections = list()
+
+            
+
+            for connection in connections:
+           
+                input, output, weight = connection
+                input_layer, input_pos = neurons_map[input]
+                output_layer, output_pos = neurons_map[output]
+
+                circle1 = neurons_layers[input_layer][input_pos]
+                circle2 = neurons_layers[output_layer][output_pos]
+                opacity = min(abs(weight) / maximal_weight, 1)
+                line_color = BLUE if weight > 0 else RED
+                start, end = get_corresponding_anchors(circle1, circle2)
+                line = Line(start, end)
+                self.update_line(line, circle1, circle2)
+                line.set_opacity(opacity)
+                line.set_stroke(line_color,1)
+                
+                line_connections.append(line)
+                if winner_index == 0:
+                    self.add(line)
+            
+            line_connections = VGroup(*line_connections)
+            winners_line_connections.append(line_connections)
+            winners_line_connections_numerical.append(connections)
+
+            new_connections_numerical = winners_line_connections_numerical[winner_index]
+            old_connections_numerical = winners_line_connections_numerical[winner_index - 1]
+            
+            new_connections_mobjects = winners_line_connections[winner_index]
+            old_connections_mobjects = winners_line_connections[winner_index - 1]
                 
             winners_inner_neuron_layers.append(VGroup(*neurons_layers))
             winners_networks_layers.append(network_layers)
@@ -210,15 +248,46 @@ class VisualizeNetworksRefactored(Scene):
                     uncommon.append(neuron)
 
             anims = list()
-            print(winners_inner_neuron_layers)
+           
             transformed_inner_neuron_layers = list()
             neurons_previous_map = list()
             transformed_inner_neuron_layers_targets = list()
+            actual_new_connections_numerical = list()
             if winner_index != 0:
+
+                connection_map_new = {
+
+                }
+                connection_map_old = {
+
+                }
+
                 for layer in new_layers:
                     transformed_inner_neuron_layers.append(list())
                     neurons_previous_map.append(list())
                     transformed_inner_neuron_layers_targets.append(list())
+
+                # mapping the existing connections to the corresponding neurons
+                for i,connection in enumerate(new_connections_numerical):
+                    input, output, weight = connection
+                    connection_map_new[input] = list()
+                    connection_map_new[output] = list()
+                    
+                for i,connection in enumerate(new_connections_numerical):
+                    input, output, weight = connection
+                    connection_map_new[output].append(i)
+                
+                for i,connection in enumerate(old_connections_numerical):
+                    input, output, weight = connection
+                    connection_map_old[input] = list()
+                    connection_map_old[output] = list()
+                    
+                for i,connection in enumerate(old_connections_numerical):
+                    input, output, weight = connection
+                    connection_map_old[output].append(i)
+                    
+                
+               
 
                 # moving existing circles and destroying the surplus ones
                 
@@ -239,25 +308,46 @@ class VisualizeNetworksRefactored(Scene):
                             transformed_inner_neuron_layers[layer_new].append(circle)
                             transformed_inner_neuron_layers_targets[layer_new].append(circle.target)
                             neurons_previous_map[layer_new].append(1)
+                            # remaking the connections between the common neurons that are going to move
+                            # and the rest of the network
+                            try:
+                                conns = connection_map_old[neuron]
+                            except:
+                                conns = list() # means the neuron doesn t have any connections
+                            
+                            for conn in conns:
+                                input, output, weight = old_connections_numerical[conn]
+                                line = old_connections_mobjects[conn]
+                                # if both input and output remain, it means the line should be updated
+                                if input in common:
+                                    actual_new_connections_numerical.append(conn)
+                                else:
+                                    # the line should be destroyed
+                                    anims.append(Uncreate(line))
+                                    
+
+                    
                         else:
                             # makes animations for uncommon neurons (Destroying them)
                             layer_old, pos_old = neurons_maps[winner_index-1][neuron]
                             destroyed_circle = winners_inner_neuron_layers[winner_index-1][layer_old][pos_old]
                             anims.append(Uncreate(destroyed_circle))
-
+                            # Destroying the surplus connections tied to the destroyed neurons
 
                 self.play(AnimationGroup(*anims))
                 self.wait(0.1)
 
                 anims = list()
+                for conn in old_connections_mobjects:
+                    anims.append(FadeOut(conn))
 
+                for conn in new_connections_mobjects:
+                    anims.append(FadeIn(conn))
+
+                # adds the missing circles from the new network 
                 for layer_num,layer in enumerate(new_layers):
                     for neuron_num,neuron in enumerate(layer):
                         if not(neuron in common):
-                            # creates missing neurons
-                            circle = Circle(color=WHITE).set_stroke(width=1.5).scale(0.07)
-                            circle.shift(start_layer_munit * RIGHT)
-                            circle.shift(step * layer_num)
                             new_circ = winners_inner_neuron_layers[winner_index][layer_num][neuron_num]
 
                             transformed_inner_neuron_layers[layer_num].append(new_circ)
@@ -286,15 +376,30 @@ class VisualizeNetworksRefactored(Scene):
 
                 self.play(AnimationGroup(*anims))
                 winners_inner_neuron_layers[winner_index] = transformed_inner_neuron_layers
-                self.wait()
+                self.wait(2)
           
 
         
 class Test(Scene):
 
+    def update_line(self, line:Line, neuron1:Circle, neuron2:Circle):
+        def shifter(mob, dt):
+            mob.put_start_and_end_on(*get_corresponding_anchors(neuron1,neuron2))
+   
+        line.add_updater(shifter)
+
     def construct(self):
-        circle1 = Circle(radius=.2)
-        pass
+        circle1 = Circle(radius=.2).shift(RIGHT)
+        circle2 = Circle(radius=.2).shift(LEFT)
+        line = Line()
+        self.update_line(line, circle1, circle2)
+        self.add(circle1,circle2,line)
+        circle1.generate_target()
+        circle1.target.shift(UP)
+        self.play(MoveToTarget(circle1))
+        self.wait()
+        self.play(Uncreate(circle1))
+        self.wait()
     
 # the format of the old layer (layernum and pos for each neuron)
 # the format of the new layer (layernum and pos for each neuron)
